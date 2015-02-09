36aeae5dccb2bfb051f1d9cd02c799e150b58b75
Brian Dupras <brian.dupras@readytalk.com>
2015-02-09 15:57:06 -0700

    WIP dockerzing the "manual install" instructions

---
+++ docker/conf/prosody.cfg.lua
@@ -0,0 +1,75 @@
+admins = { ("focus@auth." .. os.getenv("JITSI_DOMAIN")) }
+daemonize = true
+cross_domain_bosh = true;
+storage = {archive2 = "sql2"}
+sql = { driver = "SQLite3", database = "prosody.sqlite" }
+default_archive_policy = "roster"
+component_ports = { 5347 }
+
+modules_enabled = {
+    "roster"; 
+    "saslauth"; 
+    "tls"; 
+    "dialback"; 
+    "disco"; 
+    "posix"; 
+    "private"; 
+    "vcard"; 
+    "compression"; 
+    "version"; 
+    "uptime"; 
+    "time"; 
+    "ping"; 
+    "pep"; 
+    "register"; 
+    "admin_adhoc"; 
+    "bosh"; 
+    "smacks";
+    "carbons";
+    "mam";
+    "lastactivity";
+    "offline";
+    "pubsub";
+    "adhoc";
+    "websocket";
+    "http_altconnect";
+}
+
+modules_disabled = {}
+allow_registration = false
+
+ssl = {
+    key = "/etc/prosody/certs/localhost.key";
+    certificate = "/etc/prosody/certs/localhost.crt";
+}
+
+pidfile = "/var/run/prosody/prosody.pid"
+authentication = "internal_hashed"
+
+log = {
+  info = "/var/log/prosody/prosody.log"; 
+  error = "/var/log/prosody/prosody.err";
+  "*syslog";
+}
+
+VirtualHost (os.getenv("JITSI_DOMAIN"))
+  authentication = "anonymous"
+  ssl = {
+    key = ("/certs/" .. os.getenv("JITSI_DOMAIN") .. ".key");
+    certificate = ("/certs/" .. os.getenv("JITSI_DOMAIN") .. ".crt");
+  }
+
+VirtualHost ("auth." .. os.getenv("JITSI_DOMAIN") )
+  authentication = "internal_plain"
+  ssl = {
+    key = ("/certs/" .. os.getenv("JITSI_DOMAIN") .. ".key");
+    certificate = ("/certs/" .. os.getenv("JITSI_DOMAIN") .. ".crt");
+  }
+
+Component ("conference." .. os.getenv("JITSI_DOMAIN")) "muc"
+
+Component ("jitsi-videobridge." .. os.getenv("JITSI_DOMAIN"))
+    component_secret = os.getenv("VIDEOBRIDGE_SECRET")
+
+Component ("focus." .. os.getenv("JITSI_DOMAIN"))
+    component_secret = os.getenv("DOMAIN_SECRET")
---
+++ docker/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# container start script for jitsi-meet
+#
+echo starting jitsi-meet
+echo JITSI_DOMAIN       is [${JITSI_DOMAIN}]
+echo VIDEOBRIDGE_SECRET is [${VIDEOBRIDGE_SECRET}]
+echo DOMAIN_SECRET      is [${DOMAIN_SECRET}]
+echo FOCUS_SECRET       is [${FOCUS_SECRET}]
+echo CERTS ...
+ls -l /certs/${JITSI_DOMAIN}.crt /certs/${JITSI_DOMAIN}.key
+
+## real start script
+prosodyctl register focus auth.${JITSI_DOMAIN} ${FOCUS_SECRET}
+prosodyctl restart
+## end of real start script
+
+/bin/bash --login -i
+echo exiting jitsi-meet
